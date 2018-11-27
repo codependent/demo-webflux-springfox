06cf93032fff8d9b63d2f583e33de0722a76502d
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2018-11-27 15:30:51 +0100

    initial commit

---
+++ src/main/java/com/example/demo2webflux/DemoWebfluxSpringfoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo2webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableWebFlux
+@SpringBootApplication
+public class DemoWebfluxSpringfoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebfluxSpringfoxApplication.class, args);
+    }
+}
